5849b4d627d8092ad80fac56cdecbf39b4c398b0
greforce <greforce76@gmail.com>
2018-09-16 10:00:14 +0300

    initial commit

---
+++ App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>Gregory K App</Text>
+        <Text style={styles.text}>My first React Native App!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 22,
+    alignItems: 'center',
+  },
+  text: {
+    color: 'blue',
+  }
+});
